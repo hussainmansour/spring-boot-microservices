97b7ed5c4c8537d3268477f3b25a823a10d140f7
Abdulrahman-Aladdin <abdulrahmanalaaeldin1@gmail.com>
2025-03-06 01:13:00 +0200

    We have a mongoDB cache now :)

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.example.movieinfoservice.repositories.MovieRepository;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -17,17 +19,28 @@ public class MovieResource {
     private String apiKey;
 
     private RestTemplate restTemplate;
+    private MovieRepository movieRepository;
 
-    public MovieResource(RestTemplate restTemplate) {
+    public MovieResource(RestTemplate restTemplate, MovieRepository movieRepository) {
         this.restTemplate = restTemplate;
+        this.movieRepository = movieRepository;
     }
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        // check the mongoDB cache
+        Optional<Movie> cachedMovie = movieRepository.findById(movieId);
+        if (cachedMovie.isPresent()) {
+            return cachedMovie.get();
+        }
+
         // Get the movie info from TMDB
         final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
         MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
+        Movie movie = new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        // cache the result in mongoDB
+        movieRepository.save(movie);
+        return movie;
     }
 }
