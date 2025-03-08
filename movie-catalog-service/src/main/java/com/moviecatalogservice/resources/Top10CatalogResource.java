a4592e6ac3173d3748df25df9c03240b9b216a44
AH-HESHAM <ahmedhesham1652001@gmail.com>
2025-03-08 23:00:20 +0200

    moving client to catalog microservice

--- movie-catalog-service/src/main/java/com/moviecatalogservice/resources/Top10CatalogResource.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/resources/Top10CatalogResource.java
@@ -12,14 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.grpc.Top10Request;
+import com.example.grpc.Top10Response;
+import com.example.grpc.Top10ServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/catalog")
-public class MovieCatalogResource {
+@RequestMapping("/topMovies")
+public class Top10CatalogResource {
 
     private final RestTemplate restTemplate;
 
@@ -27,7 +33,7 @@ public class MovieCatalogResource {
 
     private final UserRatingService userRatingService;
 
-    public MovieCatalogResource(RestTemplate restTemplate,
+    public Top10CatalogResource(RestTemplate restTemplate,
                                 MovieInfoService movieInfoService,
                                 UserRatingService userRatingService) {
 
@@ -44,9 +50,24 @@ public class MovieCatalogResource {
      * @return CatalogItem that contains name, description and rating
      */
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable String userId) {
+    public String getCatalog(@PathVariable String userId) {
         System.out.println("Ahmed is testing id: " + userId);
-        List<Rating> ratings = userRatingService.getUserRating(userId).getRatings();
-        return ratings.stream().map(movieInfoService::getCatalogItem).collect(Collectors.toList());
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+                .build();
+
+        // Create a stub
+        Top10ServiceGrpc.Top10ServiceBlockingStub stub = Top10ServiceGrpc.newBlockingStub(channel);
+
+        // Call the RPC method
+        Top10Request request = Top10Request.newBuilder().build();
+        Top10Response response = stub.getTop10(request);
+
+        // Print the response
+        System.out.println("Response from server: " + response.getMovies(0));
+
+        // Shutdown the channel
+        channel.shutdown();
+        return "Ahmed is testing top 10 service end point" + "Response from server: " + response.getMovies(0);
     }
 }
