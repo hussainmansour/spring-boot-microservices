77a79b6f21b6a052e701e35e61c56dea789e3795
marwanesam22 <marwanesam243@gmail.com>
2025-03-09 02:36:46 +0200

    Adding Top 10 service controller

--- movie-catalog-service/src/main/java/com/moviecatalogservice/resources/Top10CatalogResource.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/resources/Top10CatalogResource.java
@@ -5,6 +5,7 @@ import com.moviecatalogservice.models.Movie;
 import com.moviecatalogservice.models.Rating;
 import com.moviecatalogservice.models.UserRating;
 import com.moviecatalogservice.services.MovieInfoService;
+import com.moviecatalogservice.services.Top10Service;
 import com.moviecatalogservice.services.UserRatingService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import com.example.grpc.Top10Request;
 import com.example.grpc.Top10Response;
 import com.example.grpc.Top10ServiceGrpc;
+import com.example.grpc.ProtoRating;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -27,47 +29,25 @@ import java.util.stream.Collectors;
 @RequestMapping("/topMovies")
 public class Top10CatalogResource {
 
-    private final RestTemplate restTemplate;
-
     private final MovieInfoService movieInfoService;
 
-    private final UserRatingService userRatingService;
+    public Top10CatalogResource(
+            MovieInfoService movieInfoService) {
 
-    public Top10CatalogResource(RestTemplate restTemplate,
-                                MovieInfoService movieInfoService,
-                                UserRatingService userRatingService) {
-
-        this.restTemplate = restTemplate;
         this.movieInfoService = movieInfoService;
-        this.userRatingService = userRatingService;
     }
 
     /**
      * Makes a call to MovieInfoService to get movieId, name and description,
      * Makes a call to RatingsService to get ratings
      * Accumulates both data to create a MovieCatalog
+     * 
      * @param userId
      * @return CatalogItem that contains name, description and rating
      */
-    @RequestMapping("/{userId}")
-    public String getCatalog(@PathVariable String userId) {
-        System.out.println("Ahmed is testing id: " + userId);
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-                .usePlaintext()
-                .build();
-
-        // Create a stub
-        Top10ServiceGrpc.Top10ServiceBlockingStub stub = Top10ServiceGrpc.newBlockingStub(channel);
-
-        // Call the RPC method
-        Top10Request request = Top10Request.newBuilder().build();
-        Top10Response response = stub.getTop10(request);
-
-        // Print the response
-        System.out.println("Response from server: " + response.getMovies(0));
-
-        // Shutdown the channel
-        channel.shutdown();
-        return "Ahmed is testing top 10 service end point" + "Response from server: " + response.getMovies(0);
+    @RequestMapping("")
+    public List<CatalogItem> getCatalog() {
+        List<ProtoRating> ratings = Top10Service.getTop10Movies();
+        return ratings.stream().map(movieInfoService::getCatalogItem).collect(Collectors.toList());
     }
 }
